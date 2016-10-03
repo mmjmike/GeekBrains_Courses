1977571f0418a54c208094ff59c9a66f63437267
mmjmike <mikhail.myshkin@phystech.edu>
2016-10-03 18:51:58 +0300

    Update main.js

--- JavaScript/Homework_4/main.js
+++ JavaScript/Homework_4/main.js
@@ -1,1 +1,23 @@
+// Minimal value
 
+function min(a, b) {
+    return (a < b) ? a : b;
+}
+
+console.log(min(20, 17));
+
+// Чёт-нечёт
+
+function evenOdd(N) {
+    if (N == 0) {
+        console.log("Чётное");
+    } else if (N == 1) {
+        console.log("Нечётное");          
+    } else if (N > 0){
+        evenOdd(N - 2);
+    } else {
+        evenOdd(N + 2);
+    }
+}
+
+evenOdd(-75)
